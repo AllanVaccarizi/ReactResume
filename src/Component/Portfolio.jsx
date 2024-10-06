a75229afcaefbb879a2ce5cf369b11fdc0f03f28
Allan Vaccarizi <allan@noot-noot.home>
2024-10-06 16:33:15 +0200

    Maj portfolio Airtable zapier

--- src/Component/Portfolio.jsx
+++ src/Component/Portfolio.jsx
@@ -5,6 +5,7 @@ import Game from "./TicTacToe" //import Tic Tac Toe
 import FL from "../images/flstudio.png"
 import MA from "../images/MarronierTopinamour.png"
 import NL from "../images/Topinamour.png"
+import AZ from "../images/AZ.png"
 import AOS from "aos" //dependecy to animate the cards
 import 'aos/dist/aos.css' //dependecy to animate the cards
 
@@ -15,22 +16,41 @@ function Portfolio(){
     return (
         <>
 
+
+        <Card style={{ padding: '1rem', margin: "1rem"}} data-aos="fade-right" data-aos-duration="1000">
+
+            <Card.Title>Growth Marketing</Card.Title>
+
+                <Card.Body>
+                Dans le cadre de ma formation avec <a href="https://oreegami.com/" target="_blank" style={{textDecoration:'None'}}>Oreegami</a>, j'ai eu le plaisir de découvrir le growth marketing automation grâce à <a href="https://www.linkedin.com/in/product-growth-scale/" target="_blank" style={{textDecoration:'None'}}>Jérémy François</a>. J'ai été introduit à des outils puissants comme Zapier et Airtable, ce qui m'a permis de créer une automatisation efficace : chaque nouvelle entrée dans ma base de données Airtable déclenche l'envoi d'un mail automatique, optimisant ainsi la gestion des contacts et le suivi des interactions. Cette introduction à l'automatisation de tâches a attisé ma curiosité, et j'ai hâte d'en découvrir davantage.
+                <br/>
+                <br/>
+                <span style={{fontSize: '2rem'}}><a href="https://sviky.mjt.lu/nl3/W5wzcqzeukVgJidvD3WtEg?m=AVYAAFCMkyEAAc4TGaAAA7Wvc54AAYKId88An5mFACv0sABm-nEdgCWfslV3RoSyLC8Tid1ijgAolVE&b=4387beb7&e=c1b25500&x=5o62Qblb5-QvilNNOZPnp3FOQgOPlJ7r4GC62A11qiU" target="_blank" style={{textDecoration:'None'}}>La newsletter</a></span>
+                </Card.Body>
+
+                <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <a href='https://topinamour.com/' target='_blank' rel="noopener noreferrer">
+                        <Image src={AZ} className="img-fluid" alt="Image de mon travail sur Airtable et Zapier" />
+                    </a>
+                </CardBody>
+
+        </Card>
         <Card style={{ padding: '1rem', margin: "1rem"}} data-aos="fade-right" data-aos-duration="1000">
 
             <Card.Title>Newsletter</Card.Title>
 
-            <Card.Body>
-            Dans le cadre de ma formation avec <a href="https://oreegami.com/" target="_blank" style={{textDecoration:'None'}}>Oreegami</a> sous la supervision de <a href="https://www.linkedin.com/in/wmoussaoui/" target='blank'style={{textDecoration:'None'}}>Wassila Moussaoui</a>, j’ai eu l’opportunité de travailler sur la création d’une <a href="https://sviky.mjt.lu/nl3/W5wzcqzeukVgJidvD3WtEg?m=AVYAAFCMkyEAAc4TGaAAA7Wvc54AAYKId88An5mFACv0sABm-nEdgCWfslV3RoSyLC8Tid1ijgAolVE&b=4387beb7&e=c1b25500&x=5o62Qblb5-QvilNNOZPnp3FOQgOPlJ7r4GC62A11qiU" target="_blank" style={{textDecoration:'None'}}>newsletter</a> fictive pour une marque. Ce projet m’a permis de mettre en pratique mes compétences en marketing digital, en collaboration avec une équipe dynamique, pour concevoir un contenu engageant et adapté aux besoins de la marque.
-            <br/>
-            <br/>
-            <span style={{fontSize: '2rem'}}><a href="https://sviky.mjt.lu/nl3/W5wzcqzeukVgJidvD3WtEg?m=AVYAAFCMkyEAAc4TGaAAA7Wvc54AAYKId88An5mFACv0sABm-nEdgCWfslV3RoSyLC8Tid1ijgAolVE&b=4387beb7&e=c1b25500&x=5o62Qblb5-QvilNNOZPnp3FOQgOPlJ7r4GC62A11qiU" target="_blank" style={{textDecoration:'None'}}>La newsletter</a></span>
-            </Card.Body>
+                <Card.Body>
+                Dans le cadre de ma formation avec <a href="https://oreegami.com/" target="_blank" style={{textDecoration:'None'}}>Oreegami</a> sous la supervision de <a href="https://www.linkedin.com/in/wmoussaoui/" target='blank'style={{textDecoration:'None'}}>Wassila Moussaoui</a>, j’ai eu l’opportunité de travailler sur la création d’une <a href="https://sviky.mjt.lu/nl3/W5wzcqzeukVgJidvD3WtEg?m=AVYAAFCMkyEAAc4TGaAAA7Wvc54AAYKId88An5mFACv0sABm-nEdgCWfslV3RoSyLC8Tid1ijgAolVE&b=4387beb7&e=c1b25500&x=5o62Qblb5-QvilNNOZPnp3FOQgOPlJ7r4GC62A11qiU" target="_blank" style={{textDecoration:'None'}}>newsletter</a> fictive pour une marque. Ce projet m’a permis de mettre en pratique mes compétences en marketing digital, en collaboration avec une équipe dynamique, pour concevoir un contenu engageant et adapté aux besoins de la marque.
+                <br/>
+                <br/>
+                <span style={{fontSize: '2rem'}}><a href="https://sviky.mjt.lu/nl3/W5wzcqzeukVgJidvD3WtEg?m=AVYAAFCMkyEAAc4TGaAAA7Wvc54AAYKId88An5mFACv0sABm-nEdgCWfslV3RoSyLC8Tid1ijgAolVE&b=4387beb7&e=c1b25500&x=5o62Qblb5-QvilNNOZPnp3FOQgOPlJ7r4GC62A11qiU" target="_blank" style={{textDecoration:'None'}}>La newsletter</a></span>
+                </Card.Body>
 
-            <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <a href='https://topinamour.com/' target='_blank' rel="noopener noreferrer">
-                    <Image src={NL} className="img-fluid" alt="Logo topinamour" />
-                </a>
-            </CardBody>
+                <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <a href='https://topinamour.com/' target='_blank' rel="noopener noreferrer">
+                        <Image src={NL} className="img-fluid" alt="Logo topinamour" />
+                    </a>
+                </CardBody>
 
         </Card>
 
@@ -38,18 +58,18 @@ function Portfolio(){
 
             <Card.Title>Marronnier</Card.Title>
 
-            <Card.Body>
-            Dans le cadre de ma formation avec Oreegami sous la supervision de <a href="https://www.linkedin.com/in/wmoussaoui/" target='blank' style={{textDecoration:'None'}}>Wassila Moussaoui</a>, j'ai également travaillé sur la réalisation d’un <a href="https://docs.google.com/spreadsheets/d/1quQl_8jDYpA4p458pURlDLgJ5nGOaS7FqrcMUya7Dso/edit?usp=sharing" target='blank' style={{textDecoration:'None'}}>marronnier</a> marketing fictif pour une marque. Ce projet m’a permis de planifier et structurer des actions marketing tout au long de l’année, en alignant les campagnes avec les moments clés pour maximiser l’impact et la cohérence de la communication.
-            <br/>
-            <br/>
-            <span style={{fontSize: '2rem'}}><a href="https://docs.google.com/spreadsheets/d/1quQl_8jDYpA4p458pURlDLgJ5nGOaS7FqrcMUya7Dso/edit?usp=sharing" target="_blank" style={{textDecoration:'None'}}>Le marronier</a></span>
-            </Card.Body>
+                <Card.Body>
+                Dans le cadre de ma formation avec Oreegami sous la supervision de <a href="https://www.linkedin.com/in/wmoussaoui/" target='blank' style={{textDecoration:'None'}}>Wassila Moussaoui</a>, j'ai également travaillé sur la réalisation d’un <a href="https://docs.google.com/spreadsheets/d/1quQl_8jDYpA4p458pURlDLgJ5nGOaS7FqrcMUya7Dso/edit?usp=sharing" target='blank' style={{textDecoration:'None'}}>marronnier</a> marketing fictif pour une marque. Ce projet m’a permis de planifier et structurer des actions marketing tout au long de l’année, en alignant les campagnes avec les moments clés pour maximiser l’impact et la cohérence de la communication.
+                <br/>
+                <br/>
+                <span style={{fontSize: '2rem'}}><a href="https://docs.google.com/spreadsheets/d/1quQl_8jDYpA4p458pURlDLgJ5nGOaS7FqrcMUya7Dso/edit?usp=sharing" target="_blank" style={{textDecoration:'None'}}>Le marronier</a></span>
+                </Card.Body>
 
-            <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <a href='https://topinamour.com/' target='_blank' rel="noopener noreferrer">
-                    <Image src={MA} className="img-fluid" alt="Image du marronnier" />
-                </a>
-            </CardBody>
+                <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <a href='https://topinamour.com/' target='_blank' rel="noopener noreferrer">
+                        <Image src={MA} className="img-fluid" alt="Image du marronnier" />
+                    </a>
+                </CardBody>
 
         </Card>
 
@@ -57,21 +77,21 @@ function Portfolio(){
 
             <Card.Title>Fl Studio</Card.Title>
 
-            <Card.Body>
-            <a href="https://fl-studio-2bjwcrdei-allan-vaccarizis-projects.vercel.app/" target='blank' style={{textDecoration:'None'}}>FL Studio</a> représente mon tout premier projet, un site conçu pour un studio de jeux vidéo. En collaboration avec le client, nous avons établi un <a href="https://docs.google.com/document/d/1WUgMZzwuuEUEuScYgNO1NS0GOMCoasARkjpBIgkjKAk/edit?usp=sharing">cahier des charges</a> détaillé et planifié des réunions régulières, tous les quinze jours, pour suivre l'avancement du site. 
-            <br />
-            <br />
-            À ce jour, la partie technique du projet est pratiquement achevée. Cependant, puisque le jeu lui-même n'est pas encore terminé, le contenu audiovisuel et textuel actuellement présent sur le site n'est ni de ma propriété ni celle du client. Il s'agit simplement de contenu temporaire en attendant que des éléments authentiques soient fournis pour rendre le site opérationnel.
-            <br />
-            <br />
-            À ce jour, le site a été entièrement conçu en HTML, CSS et JavaScript, sans l'utilisation d'aucun framework. En effet, j'ai délibérément choisi de créer mon premier site à partir de zéro afin de mieux appréhender la logique du développement web.
-             </Card.Body>
+                <Card.Body>
+                <a href="https://fl-studio-2bjwcrdei-allan-vaccarizis-projects.vercel.app/" target='blank' style={{textDecoration:'None'}}>FL Studio</a> représente mon tout premier projet, un site conçu pour un studio de jeux vidéo. En collaboration avec le client, nous avons établi un <a href="https://docs.google.com/document/d/1WUgMZzwuuEUEuScYgNO1NS0GOMCoasARkjpBIgkjKAk/edit?usp=sharing">cahier des charges</a> détaillé et planifié des réunions régulières, tous les quinze jours, pour suivre l'avancement du site. 
+                <br />
+                <br />
+                À ce jour, la partie technique du projet est pratiquement achevée. Cependant, puisque le jeu lui-même n'est pas encore terminé, le contenu audiovisuel et textuel actuellement présent sur le site n'est ni de ma propriété ni celle du client. Il s'agit simplement de contenu temporaire en attendant que des éléments authentiques soient fournis pour rendre le site opérationnel.
+                <br />
+                <br />
+                À ce jour, le site a été entièrement conçu en HTML, CSS et JavaScript, sans l'utilisation d'aucun framework. En effet, j'ai délibérément choisi de créer mon premier site à partir de zéro afin de mieux appréhender la logique du développement web.
+                </Card.Body>
 
-             <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                <a href='https://topinamour.com/' target='_blank' rel="noopener noreferrer">
-                    <Image src={FL} className="img-fluid" alt="Image du site FL Studio" />
-                </a>
-            </CardBody>
+                <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <a href='https://topinamour.com/' target='_blank' rel="noopener noreferrer">
+                        <Image src={FL} className="img-fluid" alt="Image du site FL Studio" />
+                    </a>
+                </CardBody>
 
         </Card>
 
@@ -80,6 +100,8 @@ function Portfolio(){
     
                 <Card.Body>
                     Pendant mon apprentissage du langage React, j'ai plongé dans la documentation du site, ce qui m'a permis de développer un jeu de Tic Tac Toe.
+                    <br/>
+                    <br/>
                     <span style={{color:'grey'}}>L'affichage sur téléphone peut être disfonctionnel en fonction de la taille de votre appareil</span>
                 </Card.Body>
 
--- src/Component/Profile.jsx
+++ src/Component/Profile.jsx
@@ -29,89 +29,89 @@ function Profile() {
     </Card>
 
     <Card style={{ padding: '1rem', margin: "1rem" }} data-aos="fade-left" data-aos-duration="1000">
-    <Container>
-        <Row>
-            {/* Pour les écrans plus petits, la colonne de l'image sera en premier */}
-            <Col xs={12} md={6} className="d-flex justify-content-center align-items-center mb-3 mb-md-0">
-                <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <a href='https://www.linkedin.com/in/allanvaccarizi/' target='_blank' rel="noopener noreferrer">
-                        <Image src={Pp} rounded className="img-fluid" alt="Photo de Allan Vaccarizi" />
-                    </a>
-                </CardBody>
-            </Col>
-            <Col xs={12} md={6}>
-                <Card.Title>Qui suis-je ?</Card.Title>
-                <Card.Body>
-                    Je m'appelle Allan Vaccarizi, j'ai 25 ans et je suis en reconversion professionnelle.
-                    <br />
-                    <br />
-                    Issu du milieu de la restauration, j'ai décidé de me tourner vers le marketing digital, un domaine qui m'attire par ses nombreuses possibilités. Actuellement, je suis une formation en marketing digital chez Oreegami, où j'acquiers les compétences nécessaires pour décrocher une alternance dès janvier 2025.
-                    <br />
-                    <br />
-                    Mon objectif est de m'épanouir dans ce domaine et de contribuer activement à la réussite des entreprises grâce à des stratégies digitales innovantes.
-                </Card.Body>
-            </Col>
-        </Row>
-    </Container>
-</Card>
+        <Container>
+            <Row>
+                {/* Pour les écrans plus petits, la colonne de l'image sera en premier */}
+                <Col xs={12} md={6} className="d-flex justify-content-center align-items-center mb-3 mb-md-0">
+                    <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <a href='https://www.linkedin.com/in/allanvaccarizi/' target='_blank' rel="noopener noreferrer">
+                            <Image src={Pp} rounded className="img-fluid" alt="Photo de Allan Vaccarizi" />
+                        </a>
+                    </CardBody>
+                </Col>
+                <Col xs={12} md={6}>
+                    <Card.Title>Qui suis-je ?</Card.Title>
+                    <Card.Body>
+                        Je m'appelle Allan Vaccarizi, j'ai 25 ans et je suis en reconversion professionnelle.
+                        <br />
+                        <br />
+                        Issu du milieu de la restauration, j'ai décidé de me tourner vers le marketing digital, un domaine qui m'attire par ses nombreuses possibilités. Actuellement, je suis une formation en marketing digital chez Oreegami, où j'acquiers les compétences nécessaires pour décrocher une alternance dès janvier 2025.
+                        <br />
+                        <br />
+                        Mon objectif est de m'épanouir dans ce domaine et de contribuer activement à la réussite des entreprises grâce à des stratégies digitales innovantes.
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Container>
+    </Card>
 
-<Card style={{ padding: '1rem', margin: "1rem" }} data-aos="fade-right" data-aos-duration="1000">
-    <Container>
-        <Row>
-            {/* Colonne de l'image, qui sera au-dessus du texte sur les petits écrans */}
-            <Col xs={12} md={6} className="d-flex justify-content-center align-items-center mb-3 mb-md-0">
-                <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                    <a href='https://www.linkedin.com/in/allanvaccarizi/' target='_blank' rel="noopener noreferrer">
-                        <Image src={OR} rounded className="img-fluid" alt="Photo du logo Oreegami" />
-                    </a>
-                </CardBody>
-            </Col>
+    <Card style={{ padding: '1rem', margin: "1rem" }} data-aos="fade-right" data-aos-duration="1000">
+        <Container>
+            <Row>
+                {/* Colonne de l'image, qui sera au-dessus du texte sur les petits écrans */}
+                <Col xs={12} md={6} className="d-flex justify-content-center align-items-center mb-3 mb-md-0">
+                    <CardBody style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <a href='https://www.linkedin.com/in/allanvaccarizi/' target='_blank' rel="noopener noreferrer">
+                            <Image src={OR} rounded className="img-fluid" alt="Photo du logo Oreegami" />
+                        </a>
+                    </CardBody>
+                </Col>
 
-            {/* Colonne de texte */}
-            <Col xs={12} md={6}>
-                <Card.Title>Mon projet</Card.Title>
-                <Card.Body>
-                    Actuellement inscrit chez Oreegami, je suis immergé dans un programme intensif de 3 mois, axé sur la maîtrise des outils et leviers du marketing digital, notamment le SEO, SEA, les réseaux sociaux, et le email marketing, me fournissant ainsi les bases nécessaires pour exceller dans ce domaine en constante évolution.
-                    <br /> 
-                    <br /> 
-                    En parallèle, je participe à des projets concrets et à des challenges (hackathons) qui me permettent de mettre en pratique les connaissances acquises. Ces projets sont conçus pour simuler des situations réelles, et impliquent un travail d'équipe collaboratif, favorisant l'application directe des compétences dans des environnements dynamiques et compétitifs.
-                    <br /> 
-                    <br /> 
-                    L'opportunité d'une alternance en 2025 représente pour moi un tremplin pour continuer à développer mes compétences en marketing digital. Voici les principaux domaines que j'explore au sein de cette formation : 
-                    <br /> 
-                    • Maîtrise des leviers du marketing digital : SEO, SEA, réseaux sociaux, email marketing
-                    <br /> 
-                    • Gestion de projets marketing en situation réelle : participation à des challenges et hackathons
-                    <br /> 
-                    • Mise en pratique dans des environnements collaboratifs et dynamiques
-                </Card.Body>
-            </Col>
-        </Row>
-    </Container>
-</Card>
+                {/* Colonne de texte */}
+                <Col xs={12} md={6}>
+                    <Card.Title>Mon projet</Card.Title>
+                    <Card.Body>
+                        Actuellement inscrit chez Oreegami, je suis immergé dans un programme intensif de 3 mois, axé sur la maîtrise des outils et leviers du marketing digital, notamment le SEO, SEA, les réseaux sociaux, et le email marketing, me fournissant ainsi les bases nécessaires pour exceller dans ce domaine en constante évolution.
+                        <br /> 
+                        <br /> 
+                        En parallèle, je participe à des projets concrets et à des challenges (hackathons) qui me permettent de mettre en pratique les connaissances acquises. Ces projets sont conçus pour simuler des situations réelles, et impliquent un travail d'équipe collaboratif, favorisant l'application directe des compétences dans des environnements dynamiques et compétitifs.
+                        <br /> 
+                        <br /> 
+                        L'opportunité d'une alternance en 2025 représente pour moi un tremplin pour continuer à développer mes compétences en marketing digital. Voici les principaux domaines que j'explore au sein de cette formation : 
+                        <br /> 
+                        • Maîtrise des leviers du marketing digital : SEO, SEA, réseaux sociaux, email marketing
+                        <br /> 
+                        • Gestion de projets marketing en situation réelle : participation à des challenges et hackathons
+                        <br /> 
+                        • Mise en pratique dans des environnements collaboratifs et dynamiques
+                    </Card.Body>
+                </Col>
+            </Row>
+        </Container>
+    </Card>
 
     <Card style={{ padding: '1rem', margin: "1rem"}} data-aos="fade-right" data-aos-duration="1000">
 
         <Card.Title>Mes passions</Card.Title>
 
-        <Card.Body>
+            <Card.Body>
 
-            <ListGroup>
+                <ListGroup>
 
-                <ListGroup.Item style={{ fontWeight: "bold"}}>L'informatique</ListGroup.Item>
-                    <Card.Body> Je consacre beaucoup de temps à être sur mon ordinateur, ce qui m'a d'ailleurs aidé à orienter ma carrière vers le développement web.</Card.Body>
+                    <ListGroup.Item style={{ fontWeight: "bold"}}>L'informatique</ListGroup.Item>
+                        <Card.Body> Je consacre beaucoup de temps à être sur mon ordinateur, ce qui m'a d'ailleurs aidé à orienter ma carrière vers le développement web.</Card.Body>
 
-                <ListGroup.Item style={{ fontWeight: "bold"}}>Basketball</ListGroup.Item>
-                    <Card.Body> Le basketball est un sport que je pratique régulièrement avec des amis depuis le lycée. Même si je n'ai jamais joué en club, j'apprécie toujours les moments passés sur le terrain.</Card.Body>
+                    <ListGroup.Item style={{ fontWeight: "bold"}}>Basketball</ListGroup.Item>
+                        <Card.Body> Le basketball est un sport que je pratique régulièrement avec des amis depuis le lycée. Même si je n'ai jamais joué en club, j'apprécie toujours les moments passés sur le terrain.</Card.Body>
 
-                <ListGroup.Item style={{ fontWeight: "bold"}}>Jeux vidéos</ListGroup.Item>
-                    <Card.Body> Les jeux vidéo occupent une grande partie de mon temps libre. Que ce soit sur PC ou console, ils sont pour moi un moyen de décompresser et de m'épanouir.</Card.Body>
+                    <ListGroup.Item style={{ fontWeight: "bold"}}>Jeux vidéos</ListGroup.Item>
+                        <Card.Body> Les jeux vidéo occupent une grande partie de mon temps libre. Que ce soit sur PC ou console, ils sont pour moi un moyen de décompresser et de m'épanouir.</Card.Body>
 
-                <ListGroup.Item style={{ fontWeight: "bold"}}>Esport</ListGroup.Item>
-                    <Card.Body> Je suis également passionné par l'esport. Suivre les compétitions est un passe-temps que j'apprécie énormément et qui me permet de rester connecté à cet univers.</Card.Body>
-                    
-            </ListGroup>
-        </Card.Body>
+                    <ListGroup.Item style={{ fontWeight: "bold"}}>Esport</ListGroup.Item>
+                        <Card.Body> Je suis également passionné par l'esport. Suivre les compétitions est un passe-temps que j'apprécie énormément et qui me permet de rester connecté à cet univers.</Card.Body>
+                        
+                </ListGroup>
+            </Card.Body>
     </Card>
     </>
   );
